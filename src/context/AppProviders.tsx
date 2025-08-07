09bf7d54f7584639180669a1973e6e6ac103b005
oklahomahail <davehail@me.com>
2025-08-07 09:36:07 -0500

    Fix AppContext export in AppProviders for build compatibility

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -1,7 +1,5 @@
-// src/context/AppContext.tsx
-export { useUI, UIProvider } from './ui/UIContext';
+export { useUI, UIProvider, AppContext } from './ui/UIContext';
 export { useNotifications, NotificationsProvider } from './notifications/NotificationsContext';
 export { useAppContext } from '../hooks/useAppContext';
 export { useCampaigns } from '../hooks/useCampaigns';
 export { useAnalytics } from '../hooks/useAnalytics';
-
