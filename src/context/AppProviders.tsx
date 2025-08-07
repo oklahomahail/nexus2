13bca985bfb1f8a28ed7406630e08811cc5eb4fd
oklahomahail <davehail@me.com>
2025-08-07 09:41:39 -0500

    Use AppProviders wrapper in main.tsx to fix AppContext runtime error

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -3,3 +3,14 @@ export { useNotifications, NotificationsProvider } from './notifications/Notific
 export { useAppContext } from '../hooks/useAppContext';
 export { useCampaigns } from '../hooks/useCampaigns';
 export { useAnalytics } from '../hooks/useAnalytics';
+import React from 'react';
+import { UIProvider } from './ui/UIContext';
+import { NotificationsProvider } from './notifications/NotificationsContext';
+
+export const AppProviders = ({ children }: { children: React.ReactNode }) => (
+  <NotificationsProvider>
+    <UIProvider>
+      {children}
+    </UIProvider>
+  </NotificationsProvider>
+);
