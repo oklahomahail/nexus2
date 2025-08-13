c57d9fbf4d1022d1223c7584f3f7089362c1d83f
oklahomahail <davehail@me.com>
2025-08-12 22:50:25 -0500

    chore: drop unused default React imports for new JSX runtime

--- src/pages/ClientList.tsx
+++ src/pages/ClientList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import ClientModal from "@/components/ClientModal";
 import ConfirmModal from "@/components/ui-kit/ConfirmModal";
